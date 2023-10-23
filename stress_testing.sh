33a15f4bd00472b7691dab65a58457da078b533f
Md. Sohel Rana <mrsohelcse@gmail.com>
2023-10-24 01:11:30 +0600

    Update stress_testing.sh

--- stress_testing.sh
+++ stress_testing.sh
@@ -7,25 +7,19 @@ orange=$(tput setaf 178);
 bold=$(tput bold);
 reset=$(tput sgr0);
 
-echo "Enter the name of Solution File : "
-read file
-echo "Enter the name of Brute File : "
-read file2
-
 echo "Compiling Files"
 
 g++ -std=gnu++17 -O2 -Wall -o test_gen test_gen.cpp
-g++ -std=gnu++17 -O2 -Wall -o $file $file.cpp
-g++ -std=gnu++17 -O2 -Wall -o $file2 $file2.cpp
+g++ -std=gnu++17 -O2 -Wall -o sol sol.cpp
+g++ -std=gnu++17 -O2 -Wall -o brute brute.cpp
 
 for ((i = 1; ; i++)); do
     ./test_gen > input.txt
-    if cmp -s <(./$file < input.txt) <(./$file < input.txt); then
+    if cmp -s <(./sol < input.txt) <(./brute < input.txt); then
       echo "${orange}test_case #$i: ${bold}${green}Accepted${reset}"
     else
       echo "${orange}test_case #$i: ${bold}${red}Wrong Answer${reset}"
       echo "${blue}Input: ${reset}"
-      echo ""
         cat input.txt
       echo ""
     break
